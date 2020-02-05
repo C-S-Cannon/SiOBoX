7425d6e84ad04662df6feb20e1e6b9989da6b51e
Cameron Cannon <cameron@andromeda.net>
2020-02-05 12:39:11 -0800

    configured setup.py and conformed to structure src/ model

---
+++ setup.py
@@ -0,0 +1,40 @@
+from setuptools import find_namespace_packages, setup
+
+# to understand how setuptools and many following commannds work, see
+# https://setuptools.readthedocs.io/en/latest/setuptools.html
+
+setup(
+
+    # package setup
+    name='SiOBox.testPack',  # package name
+    version='0.1',  # what version, pre release codes are 'anything' < 'final'
+                    # equivelat post finals, c = pre = preview = rc
+                    # post release codes are 'anything' > 'final'
+                    # thus 1.0 < 1.1a < 1.1pre < 1.1 < 1.1finally < 1.1g
+    packages=find_namespace_packages(where="src"),
+    # what and where are the packages, conforms to src/ layout
+    package_dir={"SiOBox": "src"},  # source directory for the given package
+    include_package_data=True,  # should data inside package be included
+    zip_safe=False,  # can package be installed from Zip?
+
+    # entry_points={'console_scripts': ['runPEPS = PEPS.command_line:main']},
+    # What is the runPEPS command in use at the above line?
+
+    # requirements
+    python_requires='>=3.3',
+    # required version of python is 3.3 or greater as defined in PEP 440
+    # necessary since this is a namespace package and 2.7 is EOL
+    setup_requires='',   # packages required to run the setup script itself
+    dependency_links='',  # URLS to be searched for setup_requires dependencies
+    install_requires='',  # packages required for the package to function
+
+    # distribution metadata
+    url='https://github.com/C-S-Cannon/SiOBoX',
+    author='Cameron Cannon',
+    author_email='c_cannon@ucsb.edu',
+    license='MIT',
+    description="This is a test package for parts of HSG-turbo",
+)
+
+# look at utilizing extras for correllated modules that are only required in
+# rare cases but bring many new dependency requirements
